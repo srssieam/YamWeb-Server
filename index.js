4ab4eb2fa56582f6d44b14cb07ec7bc814c09de3
srs-sieam <srssieam@gmail.com>
2023-11-06 08:33:15 +0600

    created a database collection in mongodb and get foodItems data from database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express= require('express');
+const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
@@ -16,32 +16,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const database = client.db("yamweb"); // provide same database name, which you have created manually. else, it will create new database
+        const serviceCollection = database.collection("foodItems"); // provide same collection name, which you have created manually. else, it will create new collection
+
+        app.get('/v1/api/foodItems', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Yam web server is running')
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`yam web server is running on http://localhost:${PORT}`)
 })
